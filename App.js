2d5120d303f836ac4f28b8dce1e1e6bf61f23b12
pavinane <pavimegan77@gmail.com>
2020-07-23 09:35:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}> Hello,pavi nane Let's Start</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.text}>Lorem ipsum dolor sit ament ..</Text>
+        <Text style={styles.text}>Lorem ipsum dolor sit ament ..</Text>
+        <Text style={styles.text}>Lorem ipsum dolor sit ament ..</Text>
+        <Text style={styles.text}>Lorem ipsum dolor sit ament ..</Text>
+        <Text style={styles.text}>Lorem ipsum dolor sit ament ..</Text>
+
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#c7c6c8',
+   alignItems:"center",
+   justifyContent:"center"
+    
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20,
+  },
+  boldText:{
+    fontWeight:'bold',
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+  },
+  text:{
+    fontWeight:"bold",
+    color:'red',
+    fontSize:10,
+    textAlign:"center",
+  }
 });
