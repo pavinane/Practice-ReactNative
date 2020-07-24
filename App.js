ac6396fabb17d2f06209bd4e31599b1abfb38dbf
pavinane <pavimegan77@gmail.com>
2020-07-24 08:55:48 +0530

    Textinput using usestate

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   const [name,setName] = useState('pavi');
   const [person,setperson] = useState({name:'keerthi', age:40});
+  const[guy,setGuy] = useState('keerthi');
+  const [year,setYear] = useState('30');
 
   const clickHander = () => {
     setName('nane');
@@ -13,9 +15,24 @@ export default function App() {
   return (
     <View style={styles.container}>
        <Text> My name is {name}</Text>
-  <Text> His name is {person.name} and his age is {person.age}</Text>
-       <View style={styles.buttonContainer}>
-         <Button title='update state' onPress={clickHander}/>
+       <Text> His name is {person.name} and his age is {person.age}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='update state' onPress={clickHander}/>
+        </View>
+        <View style={styles.Textinput}>
+          <Text> Enter name:</Text>
+          <TextInput 
+          multiline
+           style={styles.input}
+           placeholder="e.g. paviNane"
+           onChangeText={(val) => setGuy(val)}/>
+          <Text> Enter age:</Text>
+          <TextInput 
+           style={styles.input}
+           keyboardType='numeric'
+           placeholder="e.g. 40" 
+           onChangeText={(val) => setYear(val)}/>
+          <Text>name:{guy},age:{year}</Text>
        </View>
     </View>
     
@@ -26,11 +43,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems:"center",
+    // alignItems:"center",
     justifyContent:"center" 
   },
   buttonContainer:{
     marginTop:20
+  },
+  Textinput:{
+    alignItems:"center",
+    justifyContent:"center" 
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200,
   }
  
 });
+
+
