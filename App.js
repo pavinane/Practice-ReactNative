284366f3fcbe31be702c3bea25868f24bfafefc1
pavinane <pavimegan77@gmail.com>
2020-07-25 09:00:47 +0530

    list and scroll,flatlist

--- App.js
+++ App.js
@@ -1,40 +1,70 @@
 
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput,ScrollView,FlatList} from 'react-native';
 
 export default function App() {
   const [name,setName] = useState('pavi');
   const [person,setperson] = useState({name:'keerthi', age:40});
   const[guy,setGuy] = useState('keerthi');
   const [year,setYear] = useState('30');
+  const [people,setPeople] = useState([
+    {name:'vaishu', id:'1'},
+    {name:'nenePavi', id:'2'},
+    {name:'achu', id:'3'},
+    {name:'vasan', id:'4'},
+    {name:'mannikam', id:'5'},
+    {name:'annamali', id:'6'},
+  ]);
 
   const clickHander = () => {
     setName('nane');
     setperson({name:'vaishu', age:45});
   }
   return (
-    <View style={styles.container}>
-       <Text> My name is {name}</Text>
-       <Text> His name is {person.name} and his age is {person.age}</Text>
+    
+      <View style={styles.container}>
+        <Text> My name is {name}</Text>
+        <Text> His name is {person.name} and his age is {person.age}</Text>
         <View style={styles.buttonContainer}>
-          <Button title='update state' onPress={clickHander}/>
+            <Button title='update state' onPress={clickHander}/>
         </View>
-        <View style={styles.Textinput}>
-          <Text> Enter name:</Text>
-          <TextInput 
-          multiline
-           style={styles.input}
-           placeholder="e.g. paviNane"
-           onChangeText={(val) => setGuy(val)}/>
-          <Text> Enter age:</Text>
-          <TextInput 
-           style={styles.input}
-           keyboardType='numeric'
-           placeholder="e.g. 40" 
-           onChangeText={(val) => setYear(val)}/>
-          <Text>name:{guy},age:{year}</Text>
-       </View>
-    </View>
+        <View style={styles.Textinput} >
+            <Text> Enter name:</Text>
+            <TextInput 
+            multiline
+            style={styles.input}
+            placeholder="e.g. paviNane"
+            onChangeText={(val) => setGuy(val)}/>
+            <Text> Enter age:</Text>
+            <TextInput 
+            style={styles.input}
+            keyboardType='numeric'
+            placeholder="e.g. 40" 
+            onChangeText={(val) => setYear(val)}/>
+            <Text>name:{guy},age:{year}</Text>
+        </View>
+        {/* <View>
+          { people.map((item) => {
+            return(
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+            )
+          })}
+        </View>   use this method (or) get the below method are same but difference between the identical names*/}   
+      
+        <View>
+             <FlatList
+             numColumns={2}
+             keyExtractor={(item) => item.id}
+              data={people}
+              renderItem={({item}) => (
+              <Text style={styles.item}>{item.name}</Text>
+            )}
+            />
+          </View>
+      </View>
+   
     
   );
 }
@@ -43,15 +73,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems:"center",
-    justifyContent:"center" 
+    paddingTop:40,
+    paddingHorizontal:20,
+    // // alignItems:"center",
+    // // justifyContent:"center" ,
+  
+   
   },
   buttonContainer:{
-    marginTop:20
+    marginTop:20,
+    alignItems:"center",
+    // justifyContent:"center",
   },
   Textinput:{
     alignItems:"center",
-    justifyContent:"center" 
+    justifyContent:"center",
   },
   input:{
     borderWidth:1,
@@ -59,6 +95,13 @@ const styles = StyleSheet.create({
     padding:8,
     margin:10,
     width:200,
+  },
+  item:{
+    marginTop:24,
+    padding:30,
+    backgroundColor:'pink',
+    fontSize:20,
+    marginHorizontal:20
   }
  
 });
